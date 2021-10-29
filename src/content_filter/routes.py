d286cb4d52b4dbe4ac29f828ce4ce325cecc8056
Dylan <dylkinder@gmail.com>
2021-10-29 15:14:05 +0200

    async filter_twitter_content

--- src/content_filter/routes.py
+++ src/content_filter/routes.py
@@ -8,6 +8,6 @@ router = APIRouter(prefix='/api')
 
 @router.get("/filter-twitter-content", status_code=HTTP_200_OK, tags=["Twitter Content Filter"],
             response_model=FilteredContent)
-def filter_twitter_content(request: Request) -> FilteredContent:
+async def filter_twitter_content(request: Request) -> FilteredContent:
     data = await request.json()
     return FilteredContent(html_content=data.get('content'))
